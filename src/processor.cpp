5e84c5699a7e6fcdbc7ed31b4393e65c6e24f93c
Jason <jason.zhang@zenuity.com>
2020-02-23 18:13:19 -0500

    Implement the function in processor.cpp to calculate the CPU utilization

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,14 @@
 #include "processor.h"
+#include "linux_parser.h"
+#include <unistd.h>
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() {
+    long prevTotal,prevIdle, total, idle;
+    prevTotal = LinuxParser::Jiffies();
+    prevIdle = LinuxParser::IdleJiffies();
+    usleep(500); //sleep for 0.5s
+    total = LinuxParser::Jiffies();
+    idle = LinuxParser::IdleJiffies();
+    return ((total - prevTotal)- (idle - prevIdle)) / (total - prevTotal);
+}
