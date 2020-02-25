d9ccb9afd4054b7cfd5d5aebc4dccbc3ee92e515
Jason <jason.zhang@zenuity.com>
2020-02-24 22:23:28 -0500

    Implement the timer format function

--- src/format.cpp
+++ src/format.cpp
@@ -3,9 +3,20 @@
 #include "format.h"
 
 using std::string;
+using std::to_string;
 
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+    int hour,min,sec;
+    string HH,MM,SS;
+    sec = seconds % 60;
+    min = (seconds / 60) % 60;
+    hour = seconds / 3600;
+    HH = (hour > 9 ? to_string(hour) : "0"+to_string(hour));
+    MM = (min > 9 ? to_string(min) : "0"+to_string(min));
+    SS = (sec > 9 ? to_string(sec) : "0"+to_string(sec));
+    return HH + ":" + MM + ":" + SS;
+}
